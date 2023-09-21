de343aa91c339eb41766feb1e8ca284dcc8a9ecd
Adam Scriven <adam+github@scriven.ca>
2023-09-21 16:21:03 -0700

    Not sure if it's working, but the basics of the input queue handling should hopefully be displaying the button pressed on screen.

--- oet.py
+++ oet.py
@@ -8,25 +8,26 @@ import logging
 import configparser
 import json
 
-# OET Button handling
+# --- OET Button handling
 from oet_buttons import OET_Buttons
+import queue
 
-# Image creation
+# --- Image creation
+import cv2
 import numpy as np
-from types import ModuleType
 from PIL import Image
 
-# OET Screen-alone handling
-# This is only needed if a camera is not installed or is disabled in some other way,
-# when using the camera, it handles the direct-to-screen drawing as part of it's preview
-# process and we can just jump on that for our overlays
+# --- OET Screen-alone handling
+# --- This is only needed if a camera is not installed or is disabled in some other way,
+# --- when using the camera, it handles the direct-to-screen drawing as part of it's preview
+# --- process and we can just jump on that for our overlays
 from pitftmanager.libs.framebuffer import Framebuffer
 
-# used to get rpi host status
+# --- used to get rpi host status
 import subprocess
 
 
-# Constants
+# --- Constants
 RED_RGB                 = (255,0,0,255)
 BLUE_RGB                = (0,0,255,255)
 BLUE_BGR                = RED_RGB
@@ -48,16 +49,23 @@ HEARTBEAT_ORIGIN        = (120, 130)    # To the left and underneath the timesta
 HEARTBEAT_RADIUS        = 30
 RECORDING_STATUS_ORIGIN = (185, 130)
 RECORDING_STATUS_RADIUS = 30
+BUTTON_STATUS_COLOUR    = GREEN
+BUTTON_STATUS_FONT      = cv2.FONT_HERSHEY_SIMPLEX
+BUTTON_STATUS_SCALE     = 3
+BUTTON_STATUS_THICKNESS = 2
+BUTTON_STATUS_ORIGIN    = (400, 400)
 
 
-# Load Config
+# --- Load Config
 config = configparser.ConfigParser()
 config.read('oet.ini')
 
 
-# Setup variables
+# --- Setup variables
 img_w                  = 1900
 img_h                  = 1080
+#img_w                  = 720
+#img_h                  = 480
 sleep_time             = 1.0
 if config.has_option('Default','sleep'):
     sleep_time         = float(config.get('Default','sleep_time'))
@@ -70,12 +78,14 @@ timedisplay_format     = "%Y-%m-%d %H:%M:%S %z"
 if config.has_option('Default','timedisplay_format'):
     timedisplay_format = config.get('Default','timedisplay_format')
 
+
+# --- OET Variables
 framebuffer_num        = 0
-if config.has_option('Default','framebuffer_num'):
-    framebuffer_num    = config.get('Default','framebuffer_num')
+if config.has_option('OET','framebuffer_num'):
+    framebuffer_num    = int(config.get('OET','framebuffer_num'))
 
 
-# Setup some functions and logging
+# --- Setup some functions and logging
 def get_timestamp():
     now = datetime.datetime.now()
     return str(now.strftime(timestamp_format))
@@ -94,23 +104,31 @@ def logprint(lp_text):
     logging.info(lp_text)
     print(lp_text)
 
+# --- Start button queue and setup button callbacks
+input_queue = queue.Queue()
+logprint("Button Queue started.")
+
 def ui1_hold_callback():
+    input_queue.put('UI_1_HR')
     logprint("UI_1 Button Held and Released!")
 
 def ui2_hold_callback():
+    input_queue.put('UI_1_HR')
     logprint("UI_2 Button Held and Released!")
+
+def ui1_click_callback():
+    input_queue.put('UI_1_C')
+    logprint("UI_1 Button Clicked!")
+
+def ui2_click_callback():
+    input_queue.put('UI_1_C')
+    logprint("UI_2 Button Clicked!")
 
 def brm_hold_callback():
     logprint("BR- Button Held and Released!")
 
 def brp_hold_callback():
     logprint("BR+ Button Held and Released!")
-
-def ui1_click_callback():
-    logprint("UI_1 Button Clicked!")
-
-def ui2_click_callback():
-    logprint("UI_2 Button Clicked!")
 
 def brm_click_callback():
     logprint("BR- Button Clicked!")
@@ -136,8 +154,8 @@ def main():
     framebuffer = Framebuffer(framebuffer_num)
     framebuffer.start()
     framebuffer.blank()
-    logprint('Framebuffer #{} initialized: {}".format(framebuffer_num, framebuffer))
-    # NOTE: BGR Colour Mode!
+    logprint("Framebuffer #{} initialized: {}".format(framebuffer_num, framebuffer))
+    # --- NOTE: BGR Colour Mode!
 
     is_recording = False
     time_prev    = datetime.datetime.fromtimestamp(0)
@@ -147,7 +165,7 @@ def main():
         overlay_frame = np.full((img_h, img_w, 4), (0, 0, 0, 0), np.uint8)
 
         # --- Add time stamp
-        cv2.putText(overlay_frame, str(get_timedisplay()), TL_ORIGIN, TL_FONT, TL_SCALE, TL_COLOUR, TL_THICKNESS)
+        cv2.putText(overlay_frame, str(get_timedisplay()), TS_ORIGIN, TS_FONT, TS_SCALE, TS_COLOUR, TS_THICKNESS)
 
         # --- Determine raspberry pi status
         pt = subprocess.Popen(['/usr/bin/vcgencmd', 'get_throttled'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -184,6 +202,22 @@ def main():
             recording_status_colour = RED_BGR
             cv2.circle(overlay_frame, (RECORDING_STATUS_ORIGIN), RECORDING_STATUS_RADIUS, recording_status_colour, -1)
 
+        # --- Check input queue
+        input_key = ''
+        if (input_queue.qsize() > 0):
+            input_key = input_queue.get()
+            command = ''
+
+#            if input_key == 'UI_1_C':
+#            elif input_key == 'UI_1_HR':
+#            elif input_key == 'UI_2_C':
+#            elif input_key == 'UI_2_HR':
+
+            # --- Add push button status
+            cv2.putText(overlay_frame, str(input_key), BUTTON_STATUS_ORIGIN, BUTTON_STATUS_FONT, BUTTON_STATUS_SCALE, BUTTON_STATUS_COLOUR, BUTTON_STATUS_THICKNESS)
+
+
+        # --- Finally the display process!
         overlay_frame = cv2.flip(overlay_frame, 0)
         overlay_frame = cv2.cvtColor(overlay_frame, cv2.COLOR_BGR2RGB)
         overlay_frame = Image.fromarray(overlay_frame)
